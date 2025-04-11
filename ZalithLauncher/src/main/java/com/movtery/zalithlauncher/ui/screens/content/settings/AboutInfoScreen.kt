ce82d44f105f0eea0f07797e57153cff8296fd99
movte <movtery228@qq.com>
2025-04-11 10:14:56 +0800

    feat: display dependency projects on About page

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/settings/AboutInfoScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/content/settings/AboutInfoScreen.kt
@@ -1,24 +1,76 @@
 package com.movtery.zalithlauncher.ui.screens.content.settings
 
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.Launch
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import com.movtery.zalithlauncher.R
 import com.movtery.zalithlauncher.state.MutableStates
 import com.movtery.zalithlauncher.ui.base.BaseScreen
 import com.movtery.zalithlauncher.ui.screens.content.settings.layouts.SettingsBackground
 import com.movtery.zalithlauncher.utils.animation.getAnimateTween
 import com.movtery.zalithlauncher.utils.animation.getAnimateTweenBounce
+import com.movtery.zalithlauncher.utils.network.NetWorkUtils
 
 const val ABOUT_INFO_SCREEN_TAG = "AboutInfoScreen"
+
+private const val COPYRIGHT_AOSP = "Copyright © The Android Open Source Project"
+private const val COPYRIGHT_KTOR = "Copyright © 2000-2023 JetBrains s.r.o."
+private const val LICENSE_APACHE_2 = "Apache License 2.0"
+private const val URL_KTOR = "https://ktor.io"
+
+private val libraryData = listOf(
+    LibraryInfo("androidx-constraintlayout-compose", COPYRIGHT_AOSP, LICENSE_APACHE_2, "https://developer.android.com/develop/ui/compose/layouts/constraintlayout"),
+    LibraryInfo("androidx-material-icons-extended", COPYRIGHT_AOSP, LICENSE_APACHE_2, "https://developer.android.com/jetpack/androidx/releases/compose-material"),
+    LibraryInfo("androidx-material-icons-core", COPYRIGHT_AOSP, LICENSE_APACHE_2, "https://developer.android.com/jetpack/androidx/releases/compose-material"),
+    LibraryInfo("androidx-navigation-compose", COPYRIGHT_AOSP, LICENSE_APACHE_2, "https://developer.android.com/jetpack/compose/navigation"),
+    LibraryInfo("Coil Compose", "Copyright 2025 Coil Contributors", LICENSE_APACHE_2, "https://github.com/coil-kt/coil"),
+    LibraryInfo("Apache Commons Compress", null, LICENSE_APACHE_2, "https://commons.apache.org/proper/commons-compress"),
+    LibraryInfo("Apache Commons Codec", null, LICENSE_APACHE_2, "https://commons.apache.org/proper/commons-codec"),
+    LibraryInfo("Apache Commons IO", null, LICENSE_APACHE_2, "https://commons.apache.org/proper/commons-io"),
+    LibraryInfo("Gson", "Copyright © 2008 Google Inc.", LICENSE_APACHE_2, "https://github.com/google/gson"),
+    LibraryInfo("kotlinx.coroutines", "Copyright © 2000-2020 JetBrains s.r.o.", LICENSE_APACHE_2, "https://github.com/Kotlin/kotlinx.coroutines"),
+    LibraryInfo("ktor-client-cio", COPYRIGHT_KTOR, LICENSE_APACHE_2, URL_KTOR),
+    LibraryInfo("ktor-client-content-negotiation", COPYRIGHT_KTOR, LICENSE_APACHE_2, URL_KTOR),
+    LibraryInfo("ktor-client-core", COPYRIGHT_KTOR, LICENSE_APACHE_2, URL_KTOR),
+    LibraryInfo("ktor-http", COPYRIGHT_KTOR, LICENSE_APACHE_2, URL_KTOR),
+    LibraryInfo("ktor-serialization-kotlinx-json", COPYRIGHT_KTOR, LICENSE_APACHE_2, URL_KTOR),
+    LibraryInfo("OkHttp", "Copyright © 2019 Square, Inc.", LICENSE_APACHE_2, "https://github.com/square/okhttp"),
+    LibraryInfo("StringFog", "Copyright © 2016-2023, Megatron King", LICENSE_APACHE_2, "https://github.com/MegatronKing/StringFog"),
+    LibraryInfo("XZ for Java", "Copyright © The XZ for Java authors and contributors", "0BSD License", "https://tukaani.org/xz/java.html")
+)
+
+private data class LibraryInfo(
+    val name: String,
+    val copyrightInfo: String?,
+    val license: String,
+    val webUrl: String
+)
 
 @Composable
 fun AboutInfoScreen() {
@@ -37,6 +89,7 @@ fun AboutInfoScreen() {
                 animationSpec = if (isVisible) getAnimateTweenBounce() else getAnimateTween()
             )
 
+            //额外依赖库板块
             SettingsBackground(
                 modifier = Modifier
                     .offset {
@@ -46,7 +99,81 @@ fun AboutInfoScreen() {
                         )
                     }
             ) {
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        modifier = Modifier.padding(PaddingValues(horizontal = 12.dp, vertical = 8.dp)),
+                        text = stringResource(R.string.about_library_title),
+                        style = MaterialTheme.typography.titleSmall,
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                    Spacer(modifier = Modifier.width(width = 8.dp))
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .padding(horizontal = 4.dp)
+                            .fillMaxWidth(),
+                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                    )
+                    Column(modifier = Modifier.fillMaxWidth().padding(horizontal = 4.dp).padding(top = 12.dp, bottom = 8.dp)) {
+                        repeat(libraryData.size) { index ->
+                            val info = libraryData[index]
+                            LibraryInfoItem(
+                                info = info,
+                                modifier = Modifier
+                                    .padding(bottom = if (index == libraryData.size - 1) 0.dp else 12.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
+@Composable
+private fun LibraryInfoItem(
+    info: LibraryInfo,
+    modifier: Modifier = Modifier,
+    contentColor: Color = MaterialTheme.colorScheme.onSecondaryContainer,
+) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.inversePrimary,
+        contentColor = contentColor,
+        shape = MaterialTheme.shapes.large,
+        shadowElevation = 4.dp,
+        onClick = {}
+    ) {
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier.weight(1f).padding(horizontal = 12.dp, vertical = 8.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = info.name,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                info.copyrightInfo?.let { copyrightInfo ->
+                    Text(
+                        text = copyrightInfo,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                Text(
+                    text = "Licensed under the ${info.license}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+            val context = LocalContext.current
+            IconButton(
+                modifier = Modifier.align(Alignment.CenterVertically),
+                onClick = {
+                    NetWorkUtils.openLink(context, info.webUrl)
+                }
+            ) {
+                Icon(
+                    imageVector = Icons.AutoMirrored.Filled.Launch,
+                    contentDescription = null
+                )
             }
         }
     }
