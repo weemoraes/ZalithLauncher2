fc44962cbffe56b7298dc731a3f39f1070150f9d
movte <movtery228@qq.com>
2025-04-13 19:55:09 +0800

    feat: add caciocavallo (17)

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/components/Components.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/components/Components.kt
@@ -4,6 +4,8 @@ import com.movtery.zalithlauncher.R
 
 enum class Components(val component: String, val displayName: String, val summary: Int) {
     AUTH_LIBS("auth_libs", "authlib-injector", R.string.unpack_screen_authlib_injector),
+    CACIOCAVALLO("caciocavallo", "caciocavallo", R.string.unpack_screen_cacio),
+    CACIOCAVALLO17("caciocavallo17", "caciocavallo 17", R.string.unpack_screen_cacio),
     LWJGL3("lwjgl3", "LWJGL 3", R.string.unpack_screen_lwjgl),
     LAUNCHER("launcher", "Launcher Components", R.string.unpack_screen_launcher)
 }
