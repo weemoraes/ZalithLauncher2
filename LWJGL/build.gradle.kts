946745e7fc98587f8ea690ccdfa135f7b720b8ae
movte <movtery228@qq.com>
2025-04-21 14:45:51 +0800

    fix(lwjgl): The Java version is not specified

--- LWJGL/build.gradle.kts
+++ LWJGL/build.gradle.kts
@@ -27,6 +27,9 @@ tasks.jar {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(8))
+    }
 }
 
 dependencies {
