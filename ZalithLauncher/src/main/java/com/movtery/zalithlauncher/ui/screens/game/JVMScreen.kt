1a62da41870f1c3075b909f31dae2287e2329bc3
movte <movtery228@qq.com>
2025-04-29 19:11:16 +0800

    update(jvm screen): make the button semi-transparent when log output is enabled

--- ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/game/JVMScreen.kt
+++ ZalithLauncher/src/main/java/com/movtery/zalithlauncher/ui/screens/game/JVMScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
@@ -61,6 +62,7 @@ fun JVMScreen() {
 
         ButtonsLayout(
             modifier = Modifier
+                .alpha(alpha = if (enableLog) 0.5f else 1f)
                 .fillMaxSize()
                 .padding(8.dp),
             changeKeyboard = {
